810f32f668852a356c4df2d31e3636ab0630f924
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-08-16 13:48:23 +0000

    Add btn/audio on index html/js code for mute/unmute

--- main.js
+++ main.js
@@ -179,3 +179,22 @@ function animate() {
     renderer.render(scene, camera);
 }
 animate();
+
+// Mute/unmute soundtrack 
+var sound = document.getElementById('soundtrack');
+var soundtrackButton = document.getElementById('soundtrack-btn');
+
+function controlMusic() {
+    if (soundtrackButton.textContent === "Unmute") {
+        sound.muted = false;
+        soundtrackButton.textContent = "Mute";
+        sound.play()
+    }
+    else {
+        sound.muted = true;
+        soundtrackButton.textContent = "Unmute";
+    }
+}
+
+soundtrackButton.addEventListener('click',controlMusic);
+   
